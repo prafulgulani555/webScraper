36c2dba142484251b15a32b8a66556448ba2ed6a
prafulgulani555 <59774145+prafulgulani555@users.noreply.github.com>
2024-11-02 20:23:55 +0530

    completed with sqlite

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import time
 import requests
 import selectorlib
+import smtplib, ssl
+import os
+import sqlite3
 
 
-URL = "https://automated.pythonanywhere.com/tours/"
+connection = sqlite3.connect("data.db")
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 def scrape(url):
     """Scrape the page source from url"""
-    response = requests.get(url)
+    response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
 
@@ -15,26 +23,53 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email():
-    print("Email was sent.")
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "prafulgulani555@gmail.com"
+    password = os.getenv("Password")
+
+    receiver = "prafulgulani555@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
+    connection.commit()
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    print(row)
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    return rows
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
 
-    content = read(extracted)
-    if extracted != "No Upcoming Tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+        if extracted != "No upcoming tours":
+            row = read(extracted)
+            if not row:
+                store(extracted)
+                send_email("Hey new event was found")
 
 
+        time.sleep(0.05)
+
+
+
+
+connection.close()
