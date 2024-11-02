96d24f8f7ce896bc7cbf6658fe666b342f536ffe
prafulgulani555 <59774145+prafulgulani555@users.noreply.github.com>
2024-11-02 17:05:31 +0530

    extract heading

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import selectorlib
+
+
+URL = "https://automated.pythonanywhere.com/tours/"
+
+def scrape(url):
+    """Scrape the page source from url"""
+    response = requests.get(url)
+    source = response.text
+    return source
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+def send_email():
+    print("Email was sent.")
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+
+    content = read(extracted)
+    if extracted != "No Upcoming Tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
+
+
